32f69ab92d48eff16cc133ceb3c5495d58c96f21
Ryan Fung <rkfung88@gmail.com>
2020-11-14 00:15:27 -0600

    Finance Page

--- Finance.aspx.cs
+++ Finance.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace SE_Proj
 {
-    public partial class About : Page
+    public partial class Finance : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
